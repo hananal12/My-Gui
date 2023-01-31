fe9f48c07c9ebc0f7d275bb71963ad4e5d23fc2e
[M]{#ff0000}[E]{#ff7f00}[T]{#ffff00}[E]{#00ff00}[O]{#00ffff}[R]{#0000ff}[ ]{#8b00ff}[O]{#ff0000}[N]{#ff7f00}[ ]{#ffff00}[T]{#00ff00}[O]{#00ffff}[P]{#0000ff} <108976667+hananal12@users.noreply.github.com>
2023-01-31 12:43:20 +0700

    Update BloxFruits.lua

--- BloxFruits.lua
+++ BloxFruits.lua
@@ -11,10 +11,28 @@ BloxFruits:Button("Thunder-Z", function()
 end)
 
 BloxFruits:Button("Mukuro Hub", function()
-   loadstring(game:HttpGet"https://raw.githubusercontent.com/xDepressionx/Free-Script/main/AllScript.lua")()
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/xDepressionx/Free-Script/main/AllScript.lua")()
 end)
 
 BloxFruits:Button("PlayBack-X Hub", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/NeaPchX2/Playback-X-HUB/main/Protected.lua.txt"))()
 
 end)
+
+BloxFruits:Button("TP Chest", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/nuqi7/TP-Chest-Blox-Fruit/main/TPChest.lua"))()
+end)
+
+BloxFruits:Button("Black Trap Hub", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/S4nZz/bt_project/main/Games/BlacktrapScript.txt'))()
+end)
+
+BloxFruits:Button("Cat Core Hub", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Steveee11/Scripts/main/BloxFruits%20Cat-Core%20Hub"))()
+end)
+
+BloxFruits:Button("Skays Hub", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Steveee11/Scripts/main/Skays%20Hub'))()
+end)
+
+BloxFruits:Button("")
